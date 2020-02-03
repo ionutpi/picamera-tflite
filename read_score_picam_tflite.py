6cc128710c474714ca2165bd04cd7b9f0e61b8b3
ionutpi <mltrionut@yahoo.co.uk>
2020-02-03 22:07:13 +0100

    added picam read script

--- read_score_picam_tflite.py
+++ read_score_picam_tflite.py
@@ -1,2 +1,12 @@
-import picamera
+# import picamera
 import numpy as np
+
+from time import sleep
+from picamera import PiCamera
+
+camera = PiCamera()
+camera.resolution = (1024, 768)
+camera.start_preview()
+# Camera warm-up time
+sleep(2)
+camera.capture('foo.jpg')
