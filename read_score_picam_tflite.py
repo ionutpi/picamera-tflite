8488edcd4d830aa9cdb7eec02795b8e31463a996
ionutpi <mltrionut@yahoo.co.uk>
2020-02-06 20:45:48 +0000

    greyscale

--- read_score_picam_tflite.py
+++ read_score_picam_tflite.py
@@ -1,11 +1,11 @@
 import time
 import picamera
 import numpy as np
-
 with picamera.PiCamera() as camera:
     camera.resolution = (128, 128)
-    camera.color_effects = (128,128)
     camera.framerate = 24
-    time.sleep(2)
-    output = np.empty((128, 128), dtype=np.uint8)
-    camera.capture(output)
+    time.sleep(.5)
+    output = np.empty((128, 128, 3), dtype=np.uint8)
+    camera.capture(output, 'rgb')
+    output_grey = np.dot(output, [0.299, 0.587, 0.114]).reshape((128, 128, 1))
+    print(output_grey.shape)
